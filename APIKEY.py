21da107204e734ca0a2a71b4c8df2b69e6050101
erinys1296 <erinys1296@gmail.com>
2025-08-17 16:51:06 +0800

    Update APIKEY.py

--- APIKEY.py
+++ APIKEY.py
@@ -2,8 +2,6 @@
 sim_api_key= "Dr9os79UkacqMRzDPbenbp1Au6AVLHfwgzxhvesxfpkk"     
 sim_secret_key= "DvuFwczTroTYtH1JYHeZ5sNwFQ9b3mXbHvqK5PMEtFWs" 
 
-sim_api_key= "72mawBKMAvP3Y3A6xKwTwytbjsPRjTQYx8SqrYF5kX5P"     
-sim_secret_key= "57k5Y9SsPTWUasDJfDEw1MqLsJWE2zS1ykUcoWEG4t8d" 
 
 
 #正式環境API
@@ -29,3 +27,4 @@ def get_Secret(Sim):
         print("The input of Sim should be True or False.")
         return None
     
+
